3b16cf0e25992c3f9ee996cf62c188fc7f6d3aff
Stelios <stelios.papagrigoriou@gmail.com>
2014-05-18 10:35:24 +0100

    Forgot to diactivate buying when offline

--- app/scripts/directives/burgerInfo.js
+++ app/scripts/directives/burgerInfo.js
@@ -7,7 +7,12 @@ angular.module('app.directives.burgerInfo', [])
                 burger: "=",
                 type: "="
             },
-            controller: function($scope){
+            controller: function($scope, $window ){
+
+                $scope.$watch('$window.navigator.onLine', function() {
+                    $scope.isOnline = $window.navigator.onLine;
+                    console.log($scope.isOnline)
+                });
 
                 $scope.buyBurger = function(burgerId, bitcoins) {
                         // this is too easy to hack (ask to buy a burger with 0 bitcoins for example)
